015bc0c5b098ba785390d515619d1d252a6b60ad
Sahil Mahapatra <sahilm231199@gmail.com>
2024-01-09 22:21:35 +0530

    connected fabricjs with nextjs

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  webpack: (config) => {
+    config.externals.push({
+      "utf-8-validate": "commonjs utf-8-validate",
+      bufferutil: "commonjs bufferutil",
+      canvas: "commonjs canvas",
+    });
+    // config.infrastructureLogging = { debug: /PackFileCache/ };
+    return config;
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
