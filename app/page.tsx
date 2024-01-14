65775022e082fc40bae894134e65428e153bdc0c
Sahil Mahapatra <sahilm231199@gmail.com>
2024-01-14 13:16:51 +0530

    added 5 more shape options

--- app/page.tsx
+++ app/page.tsx
@@ -37,9 +37,94 @@ const App = () => {
     canvas?.requestRenderAll();
   };
 
+  const addCircle = (canvas?: fabric.Canvas) => {
+    const circle = new fabric.Circle({
+      radius: 100,
+      stroke: "#2BEBC8",
+    });
+    canvas?.add(circle);
+    canvas?.requestRenderAll();
+  };
+
+  const addTriangle = (canvas?: fabric.Canvas) => {
+    const triangle = new fabric.Triangle({
+      width: 200,
+      height: 280,
+      stroke: "#2BEBC8",
+    });
+    canvas?.add(triangle);
+    canvas?.requestRenderAll();
+  };
+  const addEllipse = (canvas?: fabric.Canvas) => {
+    const ellipse = new fabric.Ellipse({
+      rx: 100,
+      ry: 50,
+      stroke: "#2BEBC8",
+    });
+    canvas?.add(ellipse);
+    canvas?.requestRenderAll();
+  };
+  const addLine = (canvas?: fabric.Canvas) => {
+    const line = new fabric.Line([50, 100, 200, 200], {
+      stroke: "#2BEBC8",
+    });
+    canvas?.add(line);
+    canvas?.requestRenderAll();
+  };
+  const addPolygon = (canvas?: fabric.Canvas) => {
+    const polygon = new fabric.Polygon(
+      [
+        { x: 100, y: 0 },
+        { x: 200, y: 50 },
+        { x: 170, y: 200 },
+        { x: 30, y: 200 },
+        { x: 0, y: 50 },
+      ],
+      {
+        stroke: "#2BEBC8",
+      }
+    );
+    canvas?.add(polygon);
+    canvas?.requestRenderAll();
+  };
+
   return (
     <div>
-      <button onClick={() => addRect(canvas)}>Rectangle</button>
+      <select
+        className="bg-black text-white p-2 rounded"
+        onChange={(event) => {
+          switch (event.target.value) {
+            case "Rectangle":
+              addRect(canvas);
+              break;
+            case "Circle":
+              addCircle(canvas);
+              break;
+            case "Triangle":
+              addTriangle(canvas);
+              break;
+            case "Ellipse":
+              addEllipse(canvas);
+              break;
+            case "Line":
+              addLine(canvas);
+              break;
+            case "Polygon":
+              addPolygon(canvas);
+              break;
+            default:
+              break;
+          }
+        }}
+      >
+        <option value="">Select shape</option>
+        <option value="Rectangle">Rectangle</option>
+        <option value="Circle">Circle</option>
+        <option value="Triangle">Triangle</option>
+        <option value="Ellipse">Ellipse</option>
+        <option value="Line">Line</option>
+        <option value="Polygon">Polygon</option>
+      </select>
       <canvas id="canvas" />
     </div>
   );
