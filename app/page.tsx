4d54d143144e0be7515c1e7877475697f93edcf6
Sahil Mahapatra <sahilm231199@gmail.com>
2024-01-20 00:00:36 +0530

    fix: line wasn't updating its stroke color

--- app/page.tsx
+++ app/page.tsx
@@ -84,7 +84,7 @@ const App = () => {
   };
   const addLine = (canvas?: fabric.Canvas) => {
     const line = new fabric.Line([50, 100, 200, 200], {
-      stroke: "#2BEBC8",
+      stroke: strokeColor,
     });
     canvas?.add(line);
     canvas?.requestRenderAll();
